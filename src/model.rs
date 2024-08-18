ada827fda9e8873fd3fe2dec01b44e507e90b619
human9000 <human9000.2021@gmail.com>
2024-08-18 14:38:28 +0500

    implemented mlp and block entities

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,5 @@
 use burn::{module::Module, prelude::*, nn::attention};
+use nn::{LayerNormConfig, LinearConfig};
 
 #[derive(Module, Debug)]
 pub struct SelfAttention<B: Backend> {
@@ -36,7 +37,6 @@ impl<B: Backend> SelfAttention<B> {
     fn forward(&self, x: Tensor<B, 3>) -> Tensor<B, 3> {
         let shape = x.dims();
         let (b, t, c) = (shape[0], shape[1], shape[2]);
-        let _ = drop(shape);
         
         let temp = self.c_attn.forward(x).chunk(self.n_embd, 2);
         let (q, k, v) = (temp[0].clone(), temp[1].clone(), temp[2].clone());
@@ -50,7 +50,74 @@ impl<B: Backend> SelfAttention<B> {
         
         let y = self.attn.forward(mhainput);
         let y = y.context;
-        let y = self.resid_dropout.forward(self.c_proj.forward(y));
-        y
+        self.resid_dropout.forward(self.c_proj.forward(y))
+    }
+}
+
+#[derive(Module, Debug)]
+pub struct Mlp<B: Backend> {
+    c_fc: nn::Linear<B>,
+    gelu: nn::Gelu,
+    c_proj: nn::Linear<B>,
+    dropout: nn::Dropout,
+}
+
+#[derive(Config, Debug)]
+pub struct MlpConfig {
+    n_embd: usize,
+    dropout: f64,
+    bias: bool
+}
+
+impl MlpConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Mlp<B> {
+        Mlp {
+            c_fc: nn::LinearConfig::new(self.n_embd, self.n_embd * 4).with_bias(self.bias).init(device),
+            gelu: nn::Gelu::new(),
+            c_proj: nn::LinearConfig::new(self.n_embd * 4, self.n_embd).with_bias(self.bias).init(device),
+            dropout: nn::DropoutConfig::new(self.dropout).init()
+        }
+    }
+}
+
+impl<B: Backend> Mlp<B> {
+    pub fn forward(&self, x: Tensor<B, 3>) -> Tensor<B, 3> {
+        let x = self.c_fc.forward(x);
+        let x = self.gelu.forward(x);
+        let x = self.c_proj.forward(x);
+        self.dropout.forward(x)
+    }
+}
+
+#[derive(Debug, Module)]
+pub struct Block<B: Backend> {
+    ln_1: nn::LayerNorm<B>,
+    attn: SelfAttention<B>,
+    ln_2: nn::LayerNorm<B>,
+    mlp: Mlp<B>,
+}
+
+#[derive(Debug, Config)]
+pub struct BlockConfig {
+    attn_config: SelfAttentionConfig,
+    mlp_config: MlpConfig,
+}
+
+impl BlockConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Block<B> {
+        Block {
+            ln_1: nn::LayerNormConfig::new(self.attn_config.n_embd).init(device),
+            attn: self.attn_config.init(device),
+            ln_2: LayerNormConfig::new(self.attn_config.n_embd).init(device),
+            mlp: self.mlp_config.init(device)
+        }
+    }    
+}
+
+impl<B: Backend> Block<B> {
+    pub fn forward(&self, x: Tensor<B, 3>) -> Tensor<B, 3> {
+        let x = x.clone() + self.attn.forward(self.ln_1.forward(x));
+        let x = x.clone() + self.mlp.forward(self.ln_2.forward(x));
+        x
     }
 }
