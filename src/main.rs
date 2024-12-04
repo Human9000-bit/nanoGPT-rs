1d6cf47f793bd75109342df60e9bf733aea3d312
human9000 <human9000.2021@gmail.com>
2024-12-04 17:41:39 +0500

    added choice between training and inference

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use log::info;
 use tokenizers::Tokenizer;
 use train::train;
 
-pub mod args;
 pub mod config;
 pub mod data;
 pub mod inference;
@@ -21,6 +20,7 @@ pub mod model;
 pub mod train;
 
 fn main() -> Result<(), anyhow::Error> {
+    let command = std::env::args().nth(1).unwrap();
     // load tokenizer and get vocabulary size
     let vocab = Tokenizer::from_pretrained("gpt2", None).unwrap();
     let vocab_size = vocab.get_vocab_size(false);
@@ -28,7 +28,6 @@ fn main() -> Result<(), anyhow::Error> {
 
     // backend and autodiff types initialization
     type MyBackend = Wgpu<f32, i32, backend::wgpu::WgslCompiler>;
-    type AutoDiff = Autodiff<MyBackend>;
 
     // device initialization
     let device = backend::wgpu::WgpuDevice::BestAvailable;
@@ -38,29 +37,36 @@ fn main() -> Result<(), anyhow::Error> {
     info!("gpt config loaded");
 
     let artifact_dir = Path::new("model/");
-    let optimizer = AdamWConfig::new();
-
-    // initialize datasets
-    let dataset_train = DbPediaDataset::train();
-    let dataset_test = DbPediaDataset::test();
-
-    // configs initialization
     let gpt_config = inits::init_gpt_config(config["model"].as_table().unwrap(), vocab_size);
-    let train_config = init_train_config(config["train"].as_table().unwrap());
 
-    // train the model
-    // TODO: add choice between train and inference modes
-    train::<AutoDiff, DbPediaDataset, DbPediaDataset>(
-        device,
-        dataset_train,
-        dataset_test,
-        train_config,
-        artifact_dir.to_path_buf(),
-        gpt_config,
-        optimizer,
-    );
+    match command.as_str() {
+        "train" => {
+            let optimizer = AdamWConfig::new();
 
-    //infer::<MyBackend>(gpt_config, device, Some("primitive".into()));
+            // initialize datasets
+            let dataset_train = DbPediaDataset::train();
+            let dataset_test = DbPediaDataset::test();
+
+            // configs initialization
+            let train_config = init_train_config(config["train"].as_table().unwrap());
+
+            // train the model
+            // TODO: add choice between train and inference modes
+            type AutoDiff = Autodiff<MyBackend>;
+            train::<AutoDiff, DbPediaDataset, DbPediaDataset>(
+                device,
+                dataset_train,
+                dataset_test,
+                train_config,
+                artifact_dir.to_path_buf(),
+                gpt_config,
+                optimizer,
+            );
+        }
+        &_ => {crate::inference::infer::<MyBackend>(gpt_config, device, Some("primitive".into()), artifact_dir.to_path_buf())}
+    }
+
+    
 
     Ok(())
 }
