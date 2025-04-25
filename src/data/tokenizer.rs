13853d3c7f78e2142b088e193728261d51b84533
Human9000-bit <94361619+Human9000-bit@users.noreply.github.com>
2025-04-25 22:00:56 +0500

    tiktoken (#2)
    * Implemented tiktoken's cl100k_base as a tokenizer
    * Wrote automated fuzzing testing for the tokenizer

--- src/data/tokenizer.rs
+++ src/data/tokenizer.rs
@@ -1,4 +1,5 @@
 use rayon::prelude::*;
+use tiktoken_rs::CoreBPE;
 
 /// General trait for tokenizer
 pub trait Tokenizer: Send + Sync {
@@ -94,6 +95,75 @@ impl Tokenizer for GptTokenizer {
     }
 }
 
+pub struct TikTokenizer {
+    tokenizer: CoreBPE,
+}
+
+impl TikTokenizer {
+    pub fn new(tokenizer: CoreBPE) -> Self {
+        Self { tokenizer }
+    }
+}
+
+impl Default for TikTokenizer {
+    fn default() -> Self {
+        Self::new(tiktoken_rs::cl100k_base().unwrap())
+    }
+}
+
+impl Tokenizer for TikTokenizer {
+    fn encode(&self, value: &str, special_tokens: bool) -> Vec<usize> {
+        match special_tokens {
+            true => self
+                .tokenizer
+                .encode_with_special_tokens(value)
+                .into_par_iter()
+                .map(|int| int as usize)
+                .collect(),
+            false => self
+                .tokenizer
+                .encode_ordinary(value)
+                .into_par_iter()
+                .map(|int| int as usize)
+                .collect(),
+        }
+    }
+    
+    fn decode(&self, tokens: &[usize]) -> String {
+        let tokens = tokens.into_par_iter().map(|int| *int as u32).collect::<Vec<u32>>();
+        self.tokenizer.decode(tokens).unwrap()
+    }
+    
+    fn start_token(&self) -> usize {
+        todo!()
+    }
+    
+    fn start_token_value(&self) -> String {
+        todo!()
+    }
+    
+    fn end_token(&self) -> usize {
+        100257
+    }
+    
+    fn end_token_value(&self) -> String {
+        "<|endoftext|>".to_string()
+    }
+    
+    fn pad_token(&self) -> usize {
+        100256
+    }
+    
+    fn pad_token_value(&self) -> String {
+        "<|pad|>".to_string()
+    }
+    
+    fn vocab_size(&self) -> usize {
+        // i found it in tiktoken source code
+        100277
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -101,8 +171,17 @@ mod tests {
 
     quickcheck! {
         /// Test that random string is the same as encoded and decoded back
-        fn test_encode_decode(text: String) -> bool {
+        fn test_encode_decode_gpt_tokenizer(text: String) -> bool {
             let tokenizer = GptTokenizer::default();
+
+            let encoded = tokenizer.encode(text.as_str(), false);
+            let decoded = tokenizer.decode(encoded.as_slice());
+
+            text == decoded
+        }
+        
+        fn test_encode_decode_tiktoken(text: String) -> bool {
+            let tokenizer = TikTokenizer::default();
 
             let encoded = tokenizer.encode(text.as_str(), false);
             let decoded = tokenizer.decode(encoded.as_slice());
